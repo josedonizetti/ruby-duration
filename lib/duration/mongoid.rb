ae4cef9ae51887c396e6821d30693fb738db4f9a
Jose Peleteiro <jose@peleteiro.net>
2010-09-14 17:29:34 -0300

    mongodb deserialization now accepts a hash

--- lib/duration/mongoid.rb
+++ lib/duration/mongoid.rb
@@ -10,7 +10,7 @@ class Duration
     if args.is_a?(Hash)
       Duration.new(args)
     elsif args.respond_to?(:to_i)
-      Duration.new(seconds.to_i)
+      Duration.new(args.to_i)
     else
       Duration.new(0)
     end
