79f655951ba7192e3ee428a4b692be05b4bfc973
Alexandre Martins <amartinsn@gmail.com>
2012-04-16 13:27:10 -0300

    removing Serializable module from mongoid class to support Mongoid 2.4.0

--- lib/duration/mongoid.rb
+++ lib/duration/mongoid.rb
@@ -3,33 +3,31 @@ require "#{File.dirname(__FILE__)}/../duration"
 # Mongoid serialization support for Duration type.
 module Mongoid
   module Fields
-    module Serializable
-      class Duration
-        include Serializable
-        
-        # Deserialize a Duration given the amount of seconds stored by Mongodb
-        #
-        # @param [Integer, nil] duration in seconds
-        # @return [Duration] deserialized Duration
-        def deserialize(seconds)
-          return if !seconds
-          ::Duration.new(seconds)
-        end
+    class Duration
+      include Serializable
+      
+      # Deserialize a Duration given the amount of seconds stored by Mongodb
+      #
+      # @param [Integer, nil] duration in seconds
+      # @return [Duration] deserialized Duration
+      def deserialize(seconds)
+        return if !seconds
+        ::Duration.new(seconds)
+      end
 
-        # Serialize a Duration or a Hash (with duration units) or a amount of seconds to
-        # a BSON serializable type.
-        #
-        # @param [Duration, Hash, Integer] value
-        # @return [Integer] duration in seconds
-        def serialize(value)
+      # Serialize a Duration or a Hash (with duration units) or a amount of seconds to
+      # a BSON serializable type.
+      #
+      # @param [Duration, Hash, Integer] value
+      # @return [Integer] duration in seconds
+      def serialize(value)
+        return if value.blank?
+        if value.is_a?(Hash)
+          value.delete_if{|k, v| v.blank? || !::Duration::UNITS.include?(k.to_sym)}
           return if value.blank?
-          if value.is_a?(Hash)
-            value.delete_if{|k, v| v.blank? || !::Duration::UNITS.include?(k.to_sym)}
-            return if value.blank?
-            ::Duration.new(value).to_i
-          elsif value.respond_to?(:to_i)
-            value.to_i
-          end
+          ::Duration.new(value).to_i
+        elsif value.respond_to?(:to_i)
+          value.to_i
         end
       end
     end
